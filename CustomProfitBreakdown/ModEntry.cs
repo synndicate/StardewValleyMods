46b4b56277ea58c0a1ef0b3bca7d43976883d7a5
synndicate <dsynn1992@gmail.com>
2020-04-04 17:52:57 -0700

    Moving customization of profit breakdown screen into separate mod

---
+++ CustomProfitBreakdown/ModEntry.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+
+namespace Synndicate.Stardew.CustomProfitBreakdown
+{
+    public class ModEntry : Mod
+    {
+        /*****************
+        ** Fields
+        *****************/
+
+        /*****************
+        ** Public Methods
+        *****************/
+
+        public override void Entry(IModHelper helper)
+        {
+
+        }
+
+        /*****************
+        ** Private Methods
+        *****************/
+    }
+}
--- CustomProfitBreakdown/README.md
+++ CustomProfitBreakdown/README.md
@@ -1,2 +1,2 @@
-# CustomProfitBreakdown
+﻿# CustomProfitBreakdown
 Choose which items go in each category on the end of day profit breakdown screen.
